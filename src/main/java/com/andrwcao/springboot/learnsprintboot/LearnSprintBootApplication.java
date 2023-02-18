4ca388e9ee14aa759bac072189fe8f7188d1dc38
andrwcao <80874259+andrwcao@users.noreply.github.com>
2023-02-18 21:41:44 +1100

    springboot initialised

---
+++ src/main/java/com/andrwcao/springboot/learnsprintboot/LearnSprintBootApplication.java
@@ -0,0 +1,13 @@
+package com.andrwcao.springboot.learnsprintboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSprintBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSprintBootApplication.class, args);
+	}
+
+}
