036607b9db2dc3e645fc09d442d7f56176dfbaa3
andrwcao <80874259+andrwcao@users.noreply.github.com>
2023-02-27 19:37:36 +1100

    Added static filtering (field name change and ignore)

--- restful-web-services/src/main/java/com/andrwcao/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/andrwcao/rest/webservices/restfulwebservices/user/User.java
@@ -2,14 +2,20 @@ package com.andrwcao.rest.webservices.restfulwebservices.user;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 public class User {
     private Integer id;
+
     @Size(min = 2, message = "Name should have at least to characters")
+    @JsonProperty("user_name")
     private String name;
+
     @Past(message = "Birth Date should be in the past")
+    @JsonProperty("birth_date")
     private LocalDate birthDate;
 
     public User(int id, String name, LocalDate birthDate) {
