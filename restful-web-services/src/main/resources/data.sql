93e2e7730e4b6ea38d02f8d598c8757453a118b4
andrwcao <80874259+andrwcao@users.noreply.github.com>
2023-03-04 20:15:58 +1100

    Added h2 through jpa

---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO user_details(id, birth_date, name) VALUES(1, current_date(), 'Andrew');
+INSERT INTO user_details(id, birth_date, name) VALUES(2, current_date(), 'Barry');
+INSERT INTO user_details(id, birth_date, name) VALUES(3, current_date(), 'Chris')
